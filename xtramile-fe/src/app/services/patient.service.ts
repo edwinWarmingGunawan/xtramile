4bb1d6330c12ec44e386acfd641a88977fe4998f
Edwin <edwin@djavafactory.com>
2024-07-23 16:40:08 +0800

    update

--- xtramile-fe/src/app/services/patient.service.ts
+++ xtramile-fe/src/app/services/patient.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiResponse, IPatient } from '../pages/shared/models/Patient';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +11,24 @@ export class PatientService {
   apiurl = 'http://localhost:8000/api/v1/patients';
   constructor(private http: HttpClient) {}
 
-  getAllPatient(): Observable<ApiResponse<IPatient[]>> {
-    return this.http.get<ApiResponse<IPatient[]>>(`${this.apiurl+'?page=0&size=10'}`);
+  // getAllPatient(): Observable<ApiResponse<IPatient[]>> {
+  //   return this.http.get<ApiResponse<IPatient[]>>(`${this.apiurl+'?page=0&size=10'}`);
+  // }
+
+  // getAllPatients(currentPage: number, limit: number): Observable<ApiResponse<IPatient[]>> {
+  //   return this.http.get<IPatient[]>(`${this.apiurl}?page=${currentPage}&pageSize=${limit}`)
+  //     .pipe(
+  //       map((patients: IPatient[]) => {
+  //         return {
+  //           data: patients,
+  //           totalPages: patients.length // or however you calculate totalPatients
+  //         } as ApiResponse<IPatient[]>;
+  //       })
+  //     );
+  // }
+
+  getAllPatients(currentPage: number, limit: number): Observable<ApiResponse<IPatient[]>> {
+    return this.http.get<ApiResponse<IPatient[]>>(`${this.apiurl}?page=${currentPage}&pageSize=${limit}`);
   }
 
   getPatient(id: string): Observable<ApiResponse<IPatient>> {
