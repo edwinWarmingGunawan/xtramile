aa6bb7290fe98f8ed20e5e8168890d85178860fb
Edwin <edwin@djavafactory.com>
2024-07-23 17:08:29 +0800

    update

--- xtramile-be/src/main/java/com/xtramile/repository/PatientRepository.java
+++ xtramile-be/src/main/java/com/xtramile/repository/PatientRepository.java
@@ -1,10 +1,12 @@
 package com.xtramile.repository;
 
 import com.xtramile.model.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepository extends JpaRepository <Patient,Long>{
-
+    Page<Patient> findByFirstNameContainingIgnoreCase(String firstName, Pageable pageable);
 }
