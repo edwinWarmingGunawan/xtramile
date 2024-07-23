0fc6981b40da7a3fb23549792ff6d32bd848e4f2
Edwin <edwin@djavafactory.com>
2024-07-23 10:40:26 +0800

    update

--- xtramile-be/src/main/java/com/xtramile/controller/PatientController.java
+++ xtramile-be/src/main/java/com/xtramile/controller/PatientController.java
@@ -7,6 +7,10 @@ import com.xtramile.util.ResponseData;
 import com.xtramile.model.Patient;
 import com.xtramile.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,9 +30,12 @@ public class PatientController {
     }
     //get all patiens
     @GetMapping("/patients")
-    public ResponseEntity<ResponseData> getAllPatients() {
-        List<Patient> patients = patientRepository.findAll();
-        ResponseData responseData = new ResponseData(patients,"Get All Data Ok.");
+    public ResponseEntity<ResponseData> getAllPatients(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Patient> patients = patientRepository.findAll(pageable);
+        ResponseData responseData = new ResponseData(patients.getContent(), "Get All Data Ok.");
         return ResponseEntity.ok(responseData);
     }
 
